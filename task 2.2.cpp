a2982f206478e0474682fce6d7826e7092666782
Mish4a <108616348+Mish4a@users.noreply.github.com>
2022-08-15 21:36:20 +0300

    Add files via upload

---
+++ task 2.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+struct account {
+    int account_num;
+    string name;
+    double balance;
+};
+void Balance_change(account& n, double new_balance) {
+    n.balance = new_balance;
+}
+int main()
+{
+    setlocale (LC_ALL, "Russian");
+    account n;
+    double new_balance;
+    cout << "Введите номер счёта: ";
+    cin >> n.account_num;
+    cout << "Введите имя владельца: ";
+    cin >> n.name;
+    cout << "Введите баланс: ";
+    cin >> n.balance;
+    cout << "Введите новый баланс: ";
+    cin >> new_balance;
+    Balance_change(n, new_balance);
+    cout << "Ваш счёт: " << n.name << ", " << n.account_num << ", " << n.balance;
+
+}
---
+++ task2.1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+enum class months {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n=1;
+    while (n) {
+        cout << "Введите номер месяца: ";
+        cin >> n;
+        if (n > 12) {
+            cout << "Неправильный номер!" << endl;
+        }
+        else if (n == 0) {
+            cout << "До свидания!" << endl;
+        }
+        else {
+            months month = static_cast<months>(n);
+            if (month == months::January) {
+                cout << "Январь" << endl;
+            } 
+            if (month == months::February) {
+                cout << "Февраль" << endl;
+            }
+            if (month == months::March) {
+                cout << "Март" << endl;
+            }
+            if (month == months::April) {
+                cout << "Апрель" << endl;
+            }
+            if (month == months::May) {
+                cout << "Май" << endl;
+            }
+            if (month == months::June) {
+                cout << "Июнь" << endl;
+            }
+            if (month == months::July) {
+                cout << "Июль" << endl;
+            }
+            if (month == months::August) {
+                cout << "Август" << endl;
+            }
+            if (month == months::September) {
+                cout << "Сентябрь" << endl;
+            }
+            if (month == months::October) {
+                cout << "Октябрь" << endl;
+            }
+            if (month == months::November) {
+                cout << "Ноябрь" << endl;
+            }
+            if (month == months::December) {
+                cout << "Декабрь" << endl;
+            }
+                
+        }
+    }
+    return 0;
+}
